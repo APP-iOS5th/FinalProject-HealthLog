84b78d2616d0121bd078c7faa022ff5e4b8c395a
JaeSunEo <wotjs020708@gmail.com>
2024-08-12 15:35:50 +0900

    RoutineAddNameview 추가

---
+++ HealthLog/HealthLog/View/RoutineAddNameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RoutineAddNameViewController.swift
+//  HealthLog
+//
+//  Created by 어재선 on 8/12/24.
+//
+
+import UIKit
+
+class RoutineAddNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
