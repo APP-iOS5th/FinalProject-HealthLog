c3574e406f3f181dfcb0f7bd93cea43650fc9360
seokyung402 <wni402@naver.com>
2024-08-12 17:36:43 +0900

    검색바 설정 및 resultview 추가버튼 수정중

---
+++ HealthLog/HealthLog/View/Schedule/SearchResultCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultCell.swift
+//  HealthLog
+//
+//  Created by seokyung on 8/12/24.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
