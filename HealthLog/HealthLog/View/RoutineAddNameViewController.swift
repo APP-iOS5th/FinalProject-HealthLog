2c8dbc33326bb631965d6c13b6209ca1ce852068
JaeSunEo <wotjs020708@gmail.com>
2024-08-13 13:10:55 +0900

    TextField 추가 및 기존 UI Color 추가

--- HealthLog/HealthLog/View/RoutineAddNameViewController.swift
+++ HealthLog/HealthLog/View/RoutineAddNameViewController.swift
@@ -12,11 +12,23 @@ class RoutineAddNameViewController: UIViewController {
     private lazy var textLabel: UILabel = {
         let label = UILabel()
         label.text = "루틴 이름을 정해주세요."
-        label.font = UIFont.systemFont(ofSize: 20)
+        label.font =  UIFont(name: "Pretendard-bold", size: 20)
+        label.textColor = .white
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
+    private lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = UIColor(named: "ColorSecondary")
+        // 더 좋은 방법 있으면 수정
+        textField.attributedPlaceholder = NSAttributedString(string: "루틴 이름 입력", attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        textField.textColor = .white
+        return textField
+        
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +37,22 @@ class RoutineAddNameViewController: UIViewController {
     }
     
     func setupView() {
-        self.view.backgroundColor = .systemBackground
-        self.view.addSubview(textLabel)
+        self.view.backgroundColor = UIColor(named: "ColorPrimary")
         
+        
+        navigationController?.setupBarAppearance()
+        self.view.addSubview(textLabel)
+        self.view.addSubview(nameTextField)
+        self.tabBarController?.tabBar.isHidden = true
         let safeArea = self.view.safeAreaLayoutGuide
 
         NSLayoutConstraint.activate([
-            self.textLabel.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            self.textLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 36)
+            self.textLabel.topAnchor.constraint(equalTo: safeArea.topAnchor, constant:  12),
+            self.textLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor,constant: 12),
+            self.nameTextField.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 10),
+            self.nameTextField.leadingAnchor.constraint(equalTo: textLabel.leadingAnchor),
+            self.nameTextField.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -12),
+            self.nameTextField.heightAnchor.constraint(equalToConstant: 44)
             
         ])
         
