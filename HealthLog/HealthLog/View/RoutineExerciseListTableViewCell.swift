1320b5955cefd4bd505a03148a310fb690a703fc
JaeSunEo <wotjs020708@gmail.com>
2024-08-14 13:39:36 +0900

    운동추가뷰 - 셀  추가

---
+++ HealthLog/HealthLog/View/RoutineExerciseListTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  RoutineExerciseListTableViewCell.swift
+//  HealthLog
+//
+//  Created by 어재선 on 8/14/24.
+//
+
+import UIKit
+
+class RoutineExerciseListTableViewCell: UITableViewCell {
+    
+    static let cellId = "RoutineExerciseListTableViewCell"
+    
+    
+
+}
