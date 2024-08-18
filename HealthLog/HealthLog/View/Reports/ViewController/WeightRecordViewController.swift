5d76fe4ed1b9b3be4a05dad899316f976123c113
sohee <s2eeehe@gmail.com>
2024-08-18 20:57:47 +0900

    인바디 정보 버튼추가

--- HealthLog/HealthLog/View/Reports/ViewController/WeightRecordViewController.swift
+++ HealthLog/HealthLog/View/Reports/ViewController/WeightRecordViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class WeightRecordViewController: UIViewController {
 
+    private lazy var inbodyinfoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("인바디 정보 입력", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .colorSecondary
+        button.layer.cornerRadius = 12
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -18,5 +27,17 @@ class WeightRecordViewController: UIViewController {
         //배경색 변경
         view.backgroundColor = UIColor(named: "ColorPrimary")
         
+        // UI에 추가
+        view.addSubview(inbodyinfoButton)
+        
+        // Auto Layout 설정
+        inbodyinfoButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            inbodyinfoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            inbodyinfoButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            inbodyinfoButton.widthAnchor.constraint(equalToConstant: 345),
+            inbodyinfoButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
     }
 }
