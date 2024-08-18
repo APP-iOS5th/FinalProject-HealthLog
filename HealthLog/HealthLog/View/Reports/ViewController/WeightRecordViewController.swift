cc7ee3e65488fad278260baa9935f05ea703f4b6
sohee <s2eeehe@gmail.com>
2024-08-18 20:32:43 +0900

    배경색변경

--- HealthLog/HealthLog/View/Reports/ViewController/WeightRecordViewController.swift
+++ HealthLog/HealthLog/View/Reports/ViewController/WeightRecordViewController.swift
@@ -11,21 +11,12 @@ class WeightRecordViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        //배경색 변경
+        view.backgroundColor = UIColor(named: "ColorPrimary")
+        
     }
-    */
-
 }
