e50cffde057b18bbfa9fbb129516b3775c9170d3
sohee <s2eeehe@gmail.com>
2024-08-20 11:33:29 +0900

    인바디 정보 박스

--- HealthLog/HealthLog/View/Reports/ViewController/WeightRecordViewController.swift
+++ HealthLog/HealthLog/View/Reports/ViewController/WeightRecordViewController.swift
@@ -24,6 +24,10 @@ class WeightRecordViewController: UIViewController {
         return button
     }()
     
+    private lazy var weightBox = InfoBox(title: "몸무게", value: "84", unit: "kg")
+    private lazy var musclesBox = InfoBox(title: "골격근량", value: "84", unit: "kg")  // 근골격량 or 골격근량
+    private lazy var fatBox = InfoBox(title: "체지방", value: "84", unit: "kg")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -35,17 +39,94 @@ class WeightRecordViewController: UIViewController {
         
         // UI에 추가
         view.addSubview(inbodyinfoButton)
+        view.addSubview(weightBox)
+        view.addSubview(musclesBox)
+        view.addSubview(fatBox)
         
         // Auto Layout 설정
         inbodyinfoButton.translatesAutoresizingMaskIntoConstraints = false
+        weightBox.translatesAutoresizingMaskIntoConstraints = false
+        musclesBox.translatesAutoresizingMaskIntoConstraints = false
+        fatBox.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             inbodyinfoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             inbodyinfoButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             inbodyinfoButton.widthAnchor.constraint(equalToConstant: 345),
-            inbodyinfoButton.heightAnchor.constraint(equalToConstant: 44)
+            inbodyinfoButton.heightAnchor.constraint(equalToConstant: 44),
+            
+            weightBox.topAnchor.constraint(equalTo: inbodyinfoButton.bottomAnchor, constant: 13),
+            weightBox.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            weightBox.widthAnchor.constraint(equalToConstant: 94),
+            weightBox.heightAnchor.constraint(equalToConstant: 88),
+            
+            musclesBox.topAnchor.constraint(equalTo: inbodyinfoButton.bottomAnchor, constant: 13),
+            musclesBox.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            musclesBox.widthAnchor.constraint(equalToConstant: 94),
+            musclesBox.heightAnchor.constraint(equalToConstant: 88),
+            
+            fatBox.topAnchor.constraint(equalTo: inbodyinfoButton.bottomAnchor, constant: 13),
+            fatBox.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            fatBox.widthAnchor.constraint(equalToConstant: 94),
+            fatBox.heightAnchor.constraint(equalToConstant: 88)
         ])
     }
+    
+    // 인바디 정보 박스
+    private func InfoBox(title: String, value: String, unit: String) -> UIView {
+       let boxView = UIView()
+       boxView.backgroundColor = .color2F2F2F
+       boxView.layer.cornerRadius = 7
+       
+       let titleLabel = UILabel()
+       titleLabel.text = title
+       titleLabel.textColor = .white
+       titleLabel.font = UIFont.font(.pretendardSemiBold, ofSize: 14)
+       titleLabel.textAlignment = .center
+       
+       let dividerView = UIView()
+       dividerView.backgroundColor = .color3E3E3E
+    
+       let valueLabel = UILabel()
+       valueLabel.text = value
+       valueLabel.textColor = .white
+       valueLabel.font = UIFont.font(.pretendardBold, ofSize: 25)
+       valueLabel.textAlignment = .center
+       
+       let unitLabel = UILabel()
+       unitLabel.text = unit
+       unitLabel.textColor = .white
+       unitLabel.font = UIFont.font(.pretendardRegular, ofSize: 12)
+       unitLabel.textAlignment = .center
+       
+       boxView.addSubview(titleLabel)
+       boxView.addSubview(dividerView)
+       boxView.addSubview(valueLabel)
+       boxView.addSubview(unitLabel)
+       
+       titleLabel.translatesAutoresizingMaskIntoConstraints = false
+       dividerView.translatesAutoresizingMaskIntoConstraints = false
+       valueLabel.translatesAutoresizingMaskIntoConstraints = false
+       unitLabel.translatesAutoresizingMaskIntoConstraints = false
+       
+       NSLayoutConstraint.activate([
+           titleLabel.topAnchor.constraint(equalTo: boxView.topAnchor, constant: 10),
+           titleLabel.centerXAnchor.constraint(equalTo: boxView.centerXAnchor),
+           
+           dividerView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+           dividerView.leadingAnchor.constraint(equalTo: boxView.leadingAnchor, constant: 8),
+           dividerView.trailingAnchor.constraint(equalTo: boxView.trailingAnchor, constant: -8),
+           dividerView.heightAnchor.constraint(equalToConstant: 1),
+           
+           valueLabel.topAnchor.constraint(equalTo: dividerView.bottomAnchor, constant: 8),
+           valueLabel.centerXAnchor.constraint(equalTo: boxView.centerXAnchor),
+           
+           unitLabel.leadingAnchor.constraint(equalTo: valueLabel.trailingAnchor, constant: 2),
+           unitLabel.bottomAnchor.constraint(equalTo: valueLabel.bottomAnchor, constant: -3),
+       ])
+       
+       return boxView
+   }
     
     @objc private func inputModalView() {
         let vc = InputModalViewController()
@@ -90,6 +171,7 @@ class InputModalViewController: UIViewController {
         return label
     }()
     
+    // 스택뷰에서 텍스트필드 값만 huggingPriority주고 싶었지만 원하는 대로 잘 안되서 UIView로 가야하나 고민이 됩니다.
     private func createInuptStack(title: String) -> UIStackView {
      let titleLabel = UILabel()
         titleLabel.text = title
@@ -106,14 +188,14 @@ class InputModalViewController: UIViewController {
         unitLabel.textColor = .white
         unitLabel.font = UIFont.font(.pretendardSemiBold, ofSize: 16)
         
-     let stack = UIStackView(arrangedSubviews: [titleLabel, numbertextField, unitLabel])
-        stack.axis = .horizontal
-        stack.spacing = 20
-        stack.alignment = .center
-        stack.distribution = .fillEqually
-        stack.translatesAutoresizingMaskIntoConstraints = false
+     let stackView = UIStackView(arrangedSubviews: [titleLabel, numbertextField, unitLabel])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
 
-     return stack
+     return stackView
     }
     
     private lazy var weightStack = createInuptStack(title: "몸무게")
@@ -137,7 +219,7 @@ class InputModalViewController: UIViewController {
     
     private func setupUI() {
         // 배경색
-        view.backgroundColor = .colorSecondary  // 잘 안보여서 임시로 색상 바꿈 기존 1E1E1E
+        view.backgroundColor = .color1E1E1E
         
         // UI에 추가
         view.addSubview(cancelButton)
@@ -185,5 +267,12 @@ class InputModalViewController: UIViewController {
         ])
     }
     
+    @objc private func cancelButtonTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc private func completeButtonTapped() {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
