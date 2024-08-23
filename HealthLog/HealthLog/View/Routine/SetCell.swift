b75638d4f0a9a318415e9d7fbe874135b272b1d2
JaeSunEo <wotjs020708@gmail.com>
2024-08-23 17:06:39 +0900

    [style] 기존코드 명칭 변경및 코드 변경

--- HealthLog/HealthLog/View/Routine/SetCell.swift
+++ HealthLog/HealthLog/View/Routine/SetCell.swift
@@ -8,54 +8,52 @@
 import UIKit
 
 class SetCell: UICollectionViewCell {
+    static let identifier = "SetCell"
     
-    static let identifier = "RoutineExerciseStepperCollectionViewCell"
+    let setNumberLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let weightTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "무게"
+        textField.keyboardType = .numberPad
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let repsTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "횟수"
+        textField.keyboardType = .numberPad
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(setNumberLabel)
+        contentView.addSubview(weightTextField)
+        contentView.addSubview(repsTextField)
         
-        let setNumberLabel: UILabel = {
-            let label = UILabel()
-            label.translatesAutoresizingMaskIntoConstraints = false
-            return label
-        }()
-        
-        let weightTextField: UITextField = {
-            let textField = UITextField()
-            textField.placeholder = "무게"
-            textField.keyboardType = .numberPad
-            textField.borderStyle = .roundedRect
-            textField.translatesAutoresizingMaskIntoConstraints = false
-            return textField
-        }()
-        
-        let repsTextField: UITextField = {
-            let textField = UITextField()
-            textField.placeholder = "횟수"
-            textField.keyboardType = .numberPad
-            textField.borderStyle = .roundedRect
-            textField.translatesAutoresizingMaskIntoConstraints = false
-            return textField
-        }()
-        
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            contentView.addSubview(setNumberLabel)
-            contentView.addSubview(weightTextField)
-            contentView.addSubview(repsTextField)
+        NSLayoutConstraint.activate([
+            setNumberLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            setNumberLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             
-            NSLayoutConstraint.activate([
-                setNumberLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-                setNumberLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-                
-                weightTextField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-                weightTextField.leadingAnchor.constraint(equalTo: setNumberLabel.trailingAnchor, constant: 16),
-                
-                repsTextField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-                repsTextField.leadingAnchor.constraint(equalTo: weightTextField.trailingAnchor, constant: 16),
-                repsTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
-            ])
-        }
-        
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
+            weightTextField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            weightTextField.leadingAnchor.constraint(equalTo: setNumberLabel.trailingAnchor, constant: 16),
+            
+            repsTextField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            repsTextField.leadingAnchor.constraint(equalTo: weightTextField.trailingAnchor, constant: 16),
+            repsTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
+        ])
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- HealthLog/HealthLog/View/Routine/SetCountHeaderView.swift
+++ HealthLog/HealthLog/View/Routine/SetCountHeaderView.swift
@@ -8,5 +8,36 @@
 import UIKit
 
 class SetCountHeaderView: UICollectionReusableView {
+    static let identifier = "SetCountHeaderView"
         
+        let stepper: UIStepper = {
+            let stepper = UIStepper()
+            stepper.translatesAutoresizingMaskIntoConstraints = false
+            return stepper
+        }()
+        
+        let label: UILabel = {
+            let label = UILabel()
+            label.text = "세트 수"
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            addSubview(label)
+            addSubview(stepper)
+            
+            NSLayoutConstraint.activate([
+                label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+                label.centerYAnchor.constraint(equalTo: centerYAnchor),
+                
+                stepper.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+                stepper.centerYAnchor.constraint(equalTo: centerYAnchor)
+            ])
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
 }
