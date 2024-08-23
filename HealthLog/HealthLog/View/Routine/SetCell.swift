3d57b9bb26cd4569dfbe430a466ebd99083ce41d
JaeSunEo <wotjs020708@gmail.com>
2024-08-23 17:05:50 +0900

    [feat] SetCell, SetHeaderView UI 추가

---
+++ HealthLog/HealthLog/View/Routine/SetCell.swift
@@ -0,0 +1,61 @@
+//
+//  RoutineExerciseStepperCollectionViewCell.swift
+//  HealthLog
+//
+//  Created by 어재선 on 8/22/24.
+//
+
+import UIKit
+
+class SetCell: UICollectionViewCell {
+    
+    static let identifier = "RoutineExerciseStepperCollectionViewCell"
+        
+        let setNumberLabel: UILabel = {
+            let label = UILabel()
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        let weightTextField: UITextField = {
+            let textField = UITextField()
+            textField.placeholder = "무게"
+            textField.keyboardType = .numberPad
+            textField.borderStyle = .roundedRect
+            textField.translatesAutoresizingMaskIntoConstraints = false
+            return textField
+        }()
+        
+        let repsTextField: UITextField = {
+            let textField = UITextField()
+            textField.placeholder = "횟수"
+            textField.keyboardType = .numberPad
+            textField.borderStyle = .roundedRect
+            textField.translatesAutoresizingMaskIntoConstraints = false
+            return textField
+        }()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            contentView.addSubview(setNumberLabel)
+            contentView.addSubview(weightTextField)
+            contentView.addSubview(repsTextField)
+            
+            NSLayoutConstraint.activate([
+                setNumberLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+                setNumberLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                
+                weightTextField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                weightTextField.leadingAnchor.constraint(equalTo: setNumberLabel.trailingAnchor, constant: 16),
+                
+                repsTextField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+                repsTextField.leadingAnchor.constraint(equalTo: weightTextField.trailingAnchor, constant: 16),
+                repsTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
+            ])
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    
+}
---
+++ HealthLog/HealthLog/View/Routine/SetCountHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  SetCountHeaderView.swift
+//  HealthLog
+//
+//  Created by 어재선 on 8/23/24.
+//
+
+import UIKit
+
+class SetCountHeaderView: UICollectionReusableView {
+        
+}
