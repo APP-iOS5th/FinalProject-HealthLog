041c6196dcd6a567553c2fd8b595f2d35f26fe3e
JaeSunEo <wotjs020708@gmail.com>
2024-08-28 16:28:16 +0900

    [add]RoutineDetailViewController 추가

---
+++ HealthLog/HealthLog/View/Routine/RoutineDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RoutineDetailViewController.swift
+//  HealthLog
+//
+//  Created by 어재선 on 8/28/24.
+//
+
+import UIKit
+
+class RoutineDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
