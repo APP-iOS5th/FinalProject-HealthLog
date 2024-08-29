3ada33043d6d7109cf5644fc71582d68fbf5fa95
sohee <s2eeehe@gmail.com>
2024-08-29 17:47:35 +0900

    } 추가

--- HealthLog/HealthLog/View/Reports/ViewController/WeightRecordViewController.swift
+++ HealthLog/HealthLog/View/Reports/ViewController/WeightRecordViewController.swift
@@ -25,8 +25,8 @@ class WeightRecordViewController: UIViewController {
     private var cancellables = Set<AnyCancellable>()
     
     private let realm = RealmManager.shared.realm
-
-
+    
+    
     private lazy var inbodyinfoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("인바디 정보 입력", for: .normal)
@@ -96,7 +96,7 @@ class WeightRecordViewController: UIViewController {
         weightBox.translatesAutoresizingMaskIntoConstraints = false
         musclesBox.translatesAutoresizingMaskIntoConstraints = false
         fatBox.translatesAutoresizingMaskIntoConstraints = false
-
+        
         NSLayoutConstraint.activate([
             inbodyinfoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             inbodyinfoButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -139,60 +139,60 @@ class WeightRecordViewController: UIViewController {
         required init(coder: NSCoder) {
             fatalError("init(coder:)")
         }
-      
-      private func setupView(title: String, value: String, unit: String) {
-          backgroundColor = .color2F2F2F
-          layer.cornerRadius = 7
-          
-          titleLabel.text = title
-          titleLabel.textColor = .white
-          titleLabel.font = UIFont.font(.pretendardSemiBold, ofSize: 14)
-          titleLabel.textAlignment = .center
-          
-          let dividerView = UIView()
-          dividerView.backgroundColor = .color3E3E3E
-          
-          valueLabel.text = value
-          valueLabel.textColor = .white
-          valueLabel.font = UIFont.font(.pretendardBold, ofSize: 25)
-          valueLabel.textAlignment = .center
-          
-          unitLabel.text = unit
-          unitLabel.textColor = .white
-          unitLabel.font = UIFont.font(.pretendardRegular, ofSize: 12)
-          unitLabel.textAlignment = .center
-          
-          addSubview(titleLabel)
-          addSubview(dividerView)
-          addSubview(valueLabel)
-          addSubview(unitLabel)
-          
-          titleLabel.translatesAutoresizingMaskIntoConstraints = false
-          dividerView.translatesAutoresizingMaskIntoConstraints = false
-          valueLabel.translatesAutoresizingMaskIntoConstraints = false
-          unitLabel.translatesAutoresizingMaskIntoConstraints = false
-          
-          NSLayoutConstraint.activate([
-              titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 13),
-              titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-              
-              dividerView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
-              dividerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
-              dividerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
-              dividerView.heightAnchor.constraint(equalToConstant: 1),
-              
-              valueLabel.topAnchor.constraint(equalTo: dividerView.bottomAnchor, constant: 8),
-              valueLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-              
-              unitLabel.leadingAnchor.constraint(equalTo: valueLabel.trailingAnchor, constant: 2),
-              unitLabel.bottomAnchor.constraint(equalTo: valueLabel.bottomAnchor, constant: -3),
-          ])
-      }
-      
-      func updateValue(_ newValue: String) {
-          valueLabel.text = newValue
-      }
-  }
+        
+        private func setupView(title: String, value: String, unit: String) {
+            backgroundColor = .color2F2F2F
+            layer.cornerRadius = 7
+            
+            titleLabel.text = title
+            titleLabel.textColor = .white
+            titleLabel.font = UIFont.font(.pretendardSemiBold, ofSize: 14)
+            titleLabel.textAlignment = .center
+            
+            let dividerView = UIView()
+            dividerView.backgroundColor = .color3E3E3E
+            
+            valueLabel.text = value
+            valueLabel.textColor = .white
+            valueLabel.font = UIFont.font(.pretendardBold, ofSize: 25)
+            valueLabel.textAlignment = .center
+            
+            unitLabel.text = unit
+            unitLabel.textColor = .white
+            unitLabel.font = UIFont.font(.pretendardRegular, ofSize: 12)
+            unitLabel.textAlignment = .center
+            
+            addSubview(titleLabel)
+            addSubview(dividerView)
+            addSubview(valueLabel)
+            addSubview(unitLabel)
+            
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            dividerView.translatesAutoresizingMaskIntoConstraints = false
+            valueLabel.translatesAutoresizingMaskIntoConstraints = false
+            unitLabel.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 13),
+                titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+                
+                dividerView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+                dividerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+                dividerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+                dividerView.heightAnchor.constraint(equalToConstant: 1),
+                
+                valueLabel.topAnchor.constraint(equalTo: dividerView.bottomAnchor, constant: 8),
+                valueLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+                
+                unitLabel.leadingAnchor.constraint(equalTo: valueLabel.trailingAnchor, constant: 2),
+                unitLabel.bottomAnchor.constraint(equalTo: valueLabel.bottomAnchor, constant: -3),
+            ])
+        }
+        
+        func updateValue(_ newValue: String) {
+            valueLabel.text = newValue
+        }
+    }
     
     // MARK: - Actions
     @objc private func inputModalView() {
@@ -225,7 +225,7 @@ class WeightRecordViewController: UIViewController {
             }
             .store(in: &cancellables)
     }
-
+    
     
     // MARK: (원열) 특정 월 단위 데이터 fetch
     func fetchInBodyDataForMonth(year: Int, month: Int) {
@@ -244,46 +244,47 @@ class WeightRecordViewController: UIViewController {
         return Calendar.current.date(from: dateComponents) ?? Date()
     }
     
-
-
-// MARK: (youngwoo) WeightRecordViewModel
-class WeightRecordViewModel {
     
-    // MARK: (youngwoo) RealmCombine 01.
-    // RealmCombine 01. observeRealmData 함수에서 observe 쓰기 위해 사용하는 변수
-    private var inbodyNotificationToken: NotificationToken?
-    private var cancellables = Set<AnyCancellable>()
-    @Published var inbodyRecords: [InBody] = []
-    private var realm: Realm?
-     init() {
-        self.realm = RealmManager.shared.realm
+    
+    // MARK: (youngwoo) WeightRecordViewModel
+    class WeightRecordViewModel {
         
-        observeRealmData()  // inbodyNotificationToken와 observe에 의해 자동으로 실행됨
-    }
-    
-    // MARK: (youngwoo) RealmCombine 02. init에 쓸 observe 함수
-    // RealmCombine 02. inbody가 DB에서 변경될때, Published 변수에 그대로 전달하도록 세팅
-    private func observeRealmData() {
-        guard let realm = realm else { return }
+        // MARK: (youngwoo) RealmCombine 01.
+        // RealmCombine 01. observeRealmData 함수에서 observe 쓰기 위해 사용하는 변수
+        private var inbodyNotificationToken: NotificationToken?
+        private var cancellables = Set<AnyCancellable>()
+        @Published var inbodyRecords: [InBody] = []
+        private var realm: Realm?
+        init() {
+            self.realm = RealmManager.shared.realm
+            
+            observeRealmData()  // inbodyNotificationToken와 observe에 의해 자동으로 실행됨
+        }
         
-        // MARK: (youngwoo) RealmCombine 03. 데이터 불러옴
-        let results = realm.objects(InBody.self)
-            .sorted(byKeyPath: "date", ascending: false)
-    
-        // result에 담은 Realm DB 데이터를 observe로 감시, DB 값 변경시 안에 있는 실행
-        inbodyNotificationToken = results.observe { [weak self] changes in
-            switch changes {
+        // MARK: (youngwoo) RealmCombine 02. init에 쓸 observe 함수
+        // RealmCombine 02. inbody가 DB에서 변경될때, Published 변수에 그대로 전달하도록 세팅
+        private func observeRealmData() {
+            guard let realm = realm else { return }
+            
+            // MARK: (youngwoo) RealmCombine 03. 데이터 불러옴
+            let results = realm.objects(InBody.self)
+                .sorted(byKeyPath: "date", ascending: false)
+            
+            // result에 담은 Realm DB 데이터를 observe로 감시, DB 값 변경시 안에 있는 실행
+            inbodyNotificationToken = results.observe { [weak self] changes in
+                switch changes {
                 case .initial(let collection):
                     self?.inbodyRecords = Array(collection)
-
-//                    print(self?.inbodyRecords ?? [])
+                    
+                    //                    print(self?.inbodyRecords ?? [])
                     
                     
-
+                    
                 case .update(let collection, _, _, _):
                     self?.inbodyRecords = Array(collection)
                 case .error(let error):
                     print("results.observe - error: \(error)")
+                }
             }
         }
     }
