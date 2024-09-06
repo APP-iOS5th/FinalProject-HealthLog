1f303dc084db7d12e97fa977ffbb7f7d5f256115
안영우 <164759000+codeway2012@users.noreply.github.com>
2024-09-07 01:33:32 +0900

    Recover RoutineAddNameViewModel.swift

--- HealthLog/HealthLog/ViewModel/Routine/RoutineAddNameViewModel.swift
+++ HealthLog/HealthLog/ViewModel/Routine/RoutineAddNameViewModel.swift
@@ -102,7 +102,7 @@ class RoutineAddNameViewModel {
                 print("results.observe - initial")
                 self?.routines = Array(collection)
             case .update(let collection, _, _, _):
-//                print("results.observe - update")
+                print("results.observe - update")
                 self?.routines = Array(collection)
             case .error(let error):
                 print("results.observe - error: \(error)")
