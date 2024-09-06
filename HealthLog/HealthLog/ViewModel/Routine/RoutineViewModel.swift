ddd4b6f6dfd4db24eec6aa94143fdb1ea86e71ae
JaeSunEo <wotjs020708@gmail.com>
2024-09-07 01:23:24 +0900

    [주석]

--- HealthLog/HealthLog/ViewModel/Routine/RoutineViewModel.swift
+++ HealthLog/HealthLog/ViewModel/Routine/RoutineViewModel.swift
@@ -56,10 +56,10 @@ class RoutineViewModel {
         routineNotificationToken = results.observe { [weak self] changes in
             switch changes {
             case .initial(let collection):
-                print("results.observe - initial")
+//                print("results.observe - initial")
                 self?.routines = Array(collection)
             case .update(let collection, _, _, _):
-                print("results.observe - update")
+//                print("results.observe - update")
                 self?.routines = Array(collection)
             case .error(let error):
                 print("results.observe - error: \(error)")
