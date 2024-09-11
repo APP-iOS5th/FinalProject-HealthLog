0081ffa82c9941ffc0b96c165e60afeb806cae50
seokyung402 <wni402@naver.com>
2024-09-11 19:16:25 +0900

    온보딩 화면 추가

---
+++ HealthLog/HealthLog/View/Onboarding/OnboardingPageView.swift
@@ -0,0 +1,42 @@
+//
+//  OnboardingPageView.swift
+//  HealthLog
+//
+//  Created by seokyung on 9/11/24.
+//
+
+import UIKit
+
+struct OnboardingPage {
+    let image: UIImage?
+}
+
+class OnboardingPageView: UIView {
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    init(page: OnboardingPage) {
+        super.init(frame: .zero)
+        imageView.image = page.image
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
