1a3551c742b2b44c8c2856e906a8c61da3206e09
안영우 <codeway119@gmail.com>
2024-09-13 23:26:49 +0900

    Tuist 마이그레이션에 의한 폰트 에셋 Extesnion에 맞춰 Font쪽 코드 수정

--- HealthLog/Sources/View/Extension/Extension+UIFont.swift
+++ HealthLog/Sources/View/Extension/Extension+UIFont.swift
@@ -20,9 +20,25 @@ enum FontName: String {
 
 extension UIFont {
     static func font(_ style: FontName, ofSize size: CGFloat) -> UIFont {
-           guard let customFont = UIFont(name: style.rawValue, size: size) else {
-               return UIFont.systemFont(ofSize: size)
-           }
-           return customFont
-       }
+        switch style {
+            case .pretendardBlack:
+                return HealthLogFontFamily.Pretendard.black.font(size: size)
+            case .pretendardBold:
+                return HealthLogFontFamily.Pretendard.bold.font(size: size)
+            case .pretendardExtraBold:
+                return HealthLogFontFamily.Pretendard.extraBold.font(size: size)
+            case .pretendardExtraLight:
+                return HealthLogFontFamily.Pretendard.extraLight.font(size: size)
+            case .pretendardLight:
+                return HealthLogFontFamily.Pretendard.light.font(size: size)
+            case .pretendardMedium:
+                return HealthLogFontFamily.Pretendard.medium.font(size: size)
+            case .pretendardRegular:
+                return HealthLogFontFamily.Pretendard.regular.font(size: size)
+            case .pretendardSemiBold:
+                return HealthLogFontFamily.Pretendard.semiBold.font(size: size)
+            case .pretendardThin:
+                return HealthLogFontFamily.Pretendard.thin.font(size: size)
+        }
+    }
 }
