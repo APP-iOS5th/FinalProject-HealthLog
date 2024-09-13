037a0e90ac69317003f3c98bb7f7e79d5cfd272f
안영우 <codeway119@gmail.com>
2024-09-14 02:01:58 +0900

    Invalid로 겹치는 문제
    .xcconfig에서 resource의 에셋 참조를 생성해주는게 있는지, Invalid 뜸. 일단 주석처리

--- HealthLog/Sources/View/Extension/Extension+Color.swift
+++ HealthLog/Sources/View/Extension/Extension+Color.swift
@@ -8,79 +8,79 @@
 import SwiftUI
 
 extension Color {
-    static var accentColor: Color {
-        return Color(HealthLogAsset.accentColor.name)
-    }
-    
-    static var color1E1E1E: Color {
-        return Color(HealthLogAsset.color1E1E1E.name)
-    }
-    
-    static var color252525: Color {
-        return Color(HealthLogAsset.color252525.name)
-    }
-    
-    static var color2B2B2B: Color {
-        return Color(HealthLogAsset.color2B2B2B.name)
-    }
-    
-    static var color2F2F2F: Color {
-        return Color(HealthLogAsset.color2F2F2F.name)
-    }
-    
-    static var color3E3E3E: Color {
-        return Color(HealthLogAsset.color3E3E3E.name)
-    }
-    
-    static var color525252: Color {
-        return Color(HealthLogAsset.color525252.name)
-    }
-    
-    static var color5A5A5A: Color {
-        return Color(HealthLogAsset.color5A5A5A.name)
-    }
-    
-    static var color767676: Color {
-        return Color(HealthLogAsset.color767676.name)
-    }
-    
-    static var color969696: Color {
-        return Color(HealthLogAsset.color969696.name)
-    }
-    
-    static var colorAccent: Color {
-        return Color(HealthLogAsset.colorAccent.name)
-    }
-    
-    static var colorBBBDBD: Color {
-        return Color(HealthLogAsset.colorBBBDBD.name)
-    }
-    
-    static var colorPrimary: Color {
-        return Color(HealthLogAsset.colorPrimary.name)
-    }
-    
-    static var colorSecondary: Color {
-        return Color(HealthLogAsset.colorSecondary.name)
-    }
-    
-    static var colorStep1: Color {
-        return Color(HealthLogAsset.colorStep1.name)
-    }
-    
-    static var colorStep2: Color {
-        return Color(HealthLogAsset.colorStep2.name)
-    }
-    
-    static var colorStep3: Color {
-        return Color(HealthLogAsset.colorStep3.name)
-    }
-    
-    static var colorStep4: Color {
-        return Color(HealthLogAsset.colorStep4.name)
-    }
-    
-    static var colorStep5: Color {
-        return Color(HealthLogAsset.colorStep5.name)
-    }
+//    static var accentColor: Color {
+//        return Color(HealthLogAsset.accentColor.name)
+//    }
+//    
+//    static var color1E1E1E: Color {
+//        return Color(HealthLogAsset.color1E1E1E.name)
+//    }
+//    
+//    static var color252525: Color {
+//        return Color(HealthLogAsset.color252525.name)
+//    }
+//    
+//    static var color2B2B2B: Color {
+//        return Color(HealthLogAsset.color2B2B2B.name)
+//    }
+//    
+//    static var color2F2F2F: Color {
+//        return Color(HealthLogAsset.color2F2F2F.name)
+//    }
+//    
+//    static var color3E3E3E: Color {
+//        return Color(HealthLogAsset.color3E3E3E.name)
+//    }
+//    
+//    static var color525252: Color {
+//        return Color(HealthLogAsset.color525252.name)
+//    }
+//    
+//    static var color5A5A5A: Color {
+//        return Color(HealthLogAsset.color5A5A5A.name)
+//    }
+//    
+//    static var color767676: Color {
+//        return Color(HealthLogAsset.color767676.name)
+//    }
+//    
+//    static var color969696: Color {
+//        return Color(HealthLogAsset.color969696.name)
+//    }
+//    
+//    static var colorAccent: Color {
+//        return Color(HealthLogAsset.colorAccent.name)
+//    }
+//    
+//    static var colorBBBDBD: Color {
+//        return Color(HealthLogAsset.colorBBBDBD.name)
+//    }
+//    
+//    static var colorPrimary: Color {
+//        return Color(HealthLogAsset.colorPrimary.name)
+//    }
+//    
+//    static var colorSecondary: Color {
+//        return Color(HealthLogAsset.colorSecondary.name)
+//    }
+//    
+//    static var colorStep1: Color {
+//        return Color(HealthLogAsset.colorStep1.name)
+//    }
+//    
+//    static var colorStep2: Color {
+//        return Color(HealthLogAsset.colorStep2.name)
+//    }
+//    
+//    static var colorStep3: Color {
+//        return Color(HealthLogAsset.colorStep3.name)
+//    }
+//    
+//    static var colorStep4: Color {
+//        return Color(HealthLogAsset.colorStep4.name)
+//    }
+//    
+//    static var colorStep5: Color {
+//        return Color(HealthLogAsset.colorStep5.name)
+//    }
 }
--- HealthLog/Sources/View/Extension/Extension+UIColor.swift
+++ HealthLog/Sources/View/Extension/Extension+UIColor.swift
@@ -8,79 +8,79 @@
 import UIKit
 
 extension UIColor {
-    static var accentColor: UIColor {
-        return UIColor(named: HealthLogAsset.accentColor.name) ?? .clear
-    }
-    
-    static var color1E1E1E: UIColor {
-        return UIColor(named: HealthLogAsset.color1E1E1E.name) ?? .clear
-    }
-    
-    static var color252525: UIColor {
-        return UIColor(named: HealthLogAsset.color252525.name) ?? .clear
-    }
-    
-    static var color2B2B2B: UIColor {
-        return UIColor(named: HealthLogAsset.color2B2B2B.name) ?? .clear
-    }
-    
-    static var color2F2F2F: UIColor {
-        return UIColor(named: HealthLogAsset.color2F2F2F.name) ?? .clear
-    }
-    
-    static var color3E3E3E: UIColor {
-        return UIColor(named: HealthLogAsset.color3E3E3E.name) ?? .clear
-    }
-    
-    static var color525252: UIColor {
-        return UIColor(named: HealthLogAsset.color525252.name) ?? .clear
-    }
-    
-    static var color5A5A5A: UIColor {
-        return UIColor(named: HealthLogAsset.color5A5A5A.name) ?? .clear
-    }
-    
-    static var color767676: UIColor {
-        return UIColor(named: HealthLogAsset.color767676.name) ?? .clear
-    }
-    
-    static var color969696: UIColor {
-        return UIColor(named: HealthLogAsset.color969696.name) ?? .clear
-    }
-    
-    static var colorAccent: UIColor {
-        return UIColor(named: HealthLogAsset.colorAccent.name) ?? .clear
-    }
-    
-    static var colorBBBDBD: UIColor {
-        return UIColor(named: HealthLogAsset.colorBBBDBD.name) ?? .clear
-    }
-    
-    static var colorPrimary: UIColor {
-        return UIColor(named: HealthLogAsset.colorPrimary.name) ?? .clear
-    }
-    
-    static var colorSecondary: UIColor {
-        return UIColor(named: HealthLogAsset.colorSecondary.name) ?? .clear
-    }
-    
-    static var colorStep1: UIColor {
-        return UIColor(named: HealthLogAsset.colorStep1.name) ?? .clear
-    }
-    
-    static var colorStep2: UIColor {
-        return UIColor(named: HealthLogAsset.colorStep2.name) ?? .clear
-    }
-    
-    static var colorStep3: UIColor {
-        return UIColor(named: HealthLogAsset.colorStep3.name) ?? .clear
-    }
-    
-    static var colorStep4: UIColor {
-        return UIColor(named: HealthLogAsset.colorStep4.name) ?? .clear
-    }
-    
-    static var colorStep5: UIColor {
-        return UIColor(named: HealthLogAsset.colorStep5.name) ?? .clear
-    }
+//    static var accentColor: UIColor {
+//        return UIColor(named: HealthLogAsset.accentColor.name) ?? .clear
+//    }
+//    
+//    static var color1E1E1E: UIColor {
+//        return UIColor(named: HealthLogAsset.color1E1E1E.name) ?? .clear
+//    }
+//    
+//    static var color252525: UIColor {
+//        return UIColor(named: HealthLogAsset.color252525.name) ?? .clear
+//    }
+//    
+//    static var color2B2B2B: UIColor {
+//        return UIColor(named: HealthLogAsset.color2B2B2B.name) ?? .clear
+//    }
+//    
+//    static var color2F2F2F: UIColor {
+//        return UIColor(named: HealthLogAsset.color2F2F2F.name) ?? .clear
+//    }
+//    
+//    static var color3E3E3E: UIColor {
+//        return UIColor(named: HealthLogAsset.color3E3E3E.name) ?? .clear
+//    }
+//    
+//    static var color525252: UIColor {
+//        return UIColor(named: HealthLogAsset.color525252.name) ?? .clear
+//    }
+//    
+//    static var color5A5A5A: UIColor {
+//        return UIColor(named: HealthLogAsset.color5A5A5A.name) ?? .clear
+//    }
+//    
+//    static var color767676: UIColor {
+//        return UIColor(named: HealthLogAsset.color767676.name) ?? .clear
+//    }
+//    
+//    static var color969696: UIColor {
+//        return UIColor(named: HealthLogAsset.color969696.name) ?? .clear
+//    }
+//    
+//    static var colorAccent: UIColor {
+//        return UIColor(named: HealthLogAsset.colorAccent.name) ?? .clear
+//    }
+//    
+//    static var colorBBBDBD: UIColor {
+//        return UIColor(named: HealthLogAsset.colorBBBDBD.name) ?? .clear
+//    }
+//    
+//    static var colorPrimary: UIColor {
+//        return UIColor(named: HealthLogAsset.colorPrimary.name) ?? .clear
+//    }
+//    
+//    static var colorSecondary: UIColor {
+//        return UIColor(named: HealthLogAsset.colorSecondary.name) ?? .clear
+//    }
+//    
+//    static var colorStep1: UIColor {
+//        return UIColor(named: HealthLogAsset.colorStep1.name) ?? .clear
+//    }
+//    
+//    static var colorStep2: UIColor {
+//        return UIColor(named: HealthLogAsset.colorStep2.name) ?? .clear
+//    }
+//    
+//    static var colorStep3: UIColor {
+//        return UIColor(named: HealthLogAsset.colorStep3.name) ?? .clear
+//    }
+//    
+//    static var colorStep4: UIColor {
+//        return UIColor(named: HealthLogAsset.colorStep4.name) ?? .clear
+//    }
+//    
+//    static var colorStep5: UIColor {
+//        return UIColor(named: HealthLogAsset.colorStep5.name) ?? .clear
+//    }
 }
